b466aa65538cbd61ca43ce7b2ffc010f3950dcd3
Grisha Ivanetc <59262523+deCodedLife@users.noreply.github.com>
2020-06-27 00:33:03 +0300

    tests

---
+++ apis/apis.go
@@ -0,0 +1,9 @@
+package Apis
+
+func alsoNew() int {
+	return 2
+}
+
+func test() int {
+	return 0
+}
---
+++ types/types.go
@@ -0,0 +1,9 @@
+package Types
+
+func someNew() int {
+	return 1
+}
+
+func coords() int {
+	return 0
+}
